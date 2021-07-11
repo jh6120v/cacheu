8e81103bafdc5bb7e43755cf84a1748cee5e6a5f
JamesYu <jamesyu@pchomepay.com.tw>
2021-07-11 17:56:41 +0800

    Add type definition file

---
+++ types/cache.min.d.ts
@@ -0,0 +1,18 @@
+declare interface config {
+    ttl: number
+    cleanup: number
+}
+
+export class Cache {
+    static config: config;
+    static data: object;
+    constructor(config: config);
+    static create(config: object): Cache;
+    static set(key: string, value: any, expire: number): void;
+    static get(key: string): any | null;
+    static has(key: string): boolean;
+    static remove(key: string): void;
+    static removeAll(): void;
+    static isExpired(time: number): boolean;
+    static cleanup(): void;
+}
